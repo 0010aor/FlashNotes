c9299ab8d2317d4c7b992cb99f1e7a9f130b2637
Victoria Tskhondia <victoria.tskhondia@gmail.com>
2025-05-17 00:27:02 +0400

    fix: fix lint errors in api.py

--- backend/src/users/api.py
+++ backend/src/users/api.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException
 
 from src.auth.services import CurrentUser, SessionDep
 from src.core.config import settings
@@ -12,7 +12,7 @@ router = APIRouter()
 
 
 @router.get("/me", response_model=UserPublic)
-def read_user_me(request: Request, current_user: CurrentUser) -> Any:
+def read_user_me(current_user: CurrentUser) -> Any:
     """
     Get current user.
     """
